ecdf46bc25c7ebb568c16a78286f82f094da8ca0
MORGANJASON <420morganjason@gmail.com>
2023-04-29 11:10:49 +0300

    completed lab

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { username1, city1 } from "../data/user";
+
 
 function Home() {
   return (
     <div id="home">
       <h1>
-        {username} is a Web Developer from {city}
+        {username1} is a Web Developer from {city1}
       </h1>
     </div>
   );
 }
+
+export default Home;
--- src/data/user.js
+++ src/data/user.js
@@ -1,6 +1,9 @@
-const username = "Liza";
-const city = "New York";
+export function username1 () {"Liza"};
+export function city1 () {"New York"};
 
-const image = "https://i.imgur.com/mV8PQxj.gif";
+export function image1 () {"https://i.imgur.com/mV8PQxj.gif"};
 
 // export all three variables as named exports
+export function username () {'John'};
+export function city () {'San Francisco'};
+export function image2 () {"https://images.unsplash.com/photo-1682258688582-f6862ab34261?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"};
